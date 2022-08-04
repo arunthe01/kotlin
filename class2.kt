cafdb18ddee5a02afebe95edd3d9e77a36593290
arun emmidi <83764669+arunthe01@users.noreply.github.com>
2022-08-04 08:14:52 +0000

    Create class2.kt

---
+++ class2.kt
@@ -0,0 +1,17 @@
+fun main() {
+//     var i = 9
+//     var k: Int = 78
+    var m : Int = Int.MIN_VALUE;
+    var m1 : Float = Float.MAX_VALUE;
+    var i=8
+    println("$i+5+9+8")
+    println("Hello, world!!!${i+5+9+8}")
+    println("Int min "+m)
+    println("Float max "+m1)
+    
+    val tr : Boolean = true
+    val fl : Boolean = false
+    
+    println(tr);
+    println(fl)    
+}
