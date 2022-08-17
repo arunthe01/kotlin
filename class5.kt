f0b6aceb4dd9747ae83d6428f5f5004e7cbe88ab
arun emmidi <83764669+arunthe01@users.noreply.github.com>
2022-08-17 07:33:16 +0000

    Create class5.kt

---
+++ class5.kt
@@ -0,0 +1,68 @@
+    var q: Int = -8
+    
+//     if(q>0)
+//     	println("$q is positive")
+//     else
+//     	println("$q is negative")
+    
+    
+    
+    var p :Int = 90
+    
+    
+    if(p >= 90 && p<101){
+        println("yay A Grade");
+    }
+    
+    else if(p >= 80 && p<90){
+        println(" B Grade, Good Try.");
+    }
+    
+    else if(p >= 60 && p<80){
+        println("C Grade , Keep trying!");
+    }
+    
+    else{
+        
+        println("Failed!!!, Focus on studies");
+    }
+    
+    var a :Int = 90
+    var b :Int = 290
+    var c :Int = 190
+    
+    
+    if(a>b ){
+        if(a>c){
+            println("$a is greater")
+        }
+    }
+    
+  else if(b>a )
+    {
+        if(b>c){
+            println("$b is greater")
+        }
+    }
+    
+    else{
+        println("$c is greater")
+    }
+    
+    
+    var z = 4
+    
+//     when(z){
+//         'a','e','i','o','u','A','E','I','O','U'-> println("you've Entered a vowel")
+//         else ->println("you've Entered a consonent")
+//     }
+//     
+//     
+when(z){
+//         in 0..9 -> println("in between 0 to 9 ")
+        is Int-> println("it's an Integer")
+        
+    }    
+    
+    
+    
