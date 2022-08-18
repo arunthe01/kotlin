612c5e182fda906ef49a34873947e7e9902a13af
arun emmidi <83764669+arunthe01@users.noreply.github.com>
2022-08-18 07:36:24 +0000

    Create class6.kt

---
+++ class6.kt
@@ -0,0 +1,65 @@
+     fun isodd(x:Int):Boolean{
+        return x%2 !=0 
+    }
+
+
+    fun iseve(x:Int):Boolean{
+        return x%2 ==0
+    }
+    
+    
+//     println("is odd:${isodd(2)}")
+    
+//     println("is even:${iseve(2)}")
+//     
+//      when{
+//         isodd(2)-> print("odd")
+//         iseve(2)->print("even")
+//     }
+     
+     //loops in kotlin
+     
+     
+     //while
+     //
+     var n : Int = 1
+     
+//      while(n<11){
+//          println(n)
+//          n++
+//      }
+     
+     // do while 
+     
+     
+     
+//      do{
+//          println(n)
+//          n++
+//      } while(n<11)
+ 
+//      for loop
+         
+         
+// for(n in 10 downTo 1 step 2){
+
+//         println(n);
+//     }
+
+
+// var arr = arrayOf("A","di","Love Lassan")
+    
+//    for(i in arr){
+//         println(i);
+//     }
+   
+//    for(i in arr.indices){
+//        println(arr[i])
+//    }
+
+    var arr = listOf(10,"20",30,40,50)
+
+    for(i in arr){
+        println(i);
+    }
+    
