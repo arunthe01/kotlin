0f847f226bf0d20db7332dc44371da850f757f7c
arun emmidi <83764669+arunthe01@users.noreply.github.com>
2022-08-25 07:49:03 +0000

    Create class9.kt

---
+++ class9.kt
@@ -0,0 +1,35 @@
+//      println(printn(10))
+//     val a = { println("Hey there " )};
+//     a()
+//     
+//     var b :(Int,Int) ->Int = {x,y-> (x+y)}
+   
+//    b(3,4)
+   
+   
+//    var a = {x:Int, y:Int -> x+y}
+    
+    
+//     var b:(Int,Int)->Unit = {x,y->x+y}
+    
+//     println(a(3,2));
+//     println(b(3,4));
+    
+    //Lamda expression with no args
+    
+//     var e = {"hello"};
+//     println(e());
+    
+//    var f:()->String = {"Hello"};
+    
+//     var g:(Int,Int)->String = {x,y->(x+y).toString()}
+    
+//     println(f())
+//     println(g(2,4));
+//     
+// IT OPEARATOR
+  
+//     val h:String.(Int)->String = {this+it}
+    
+//     println("Arun".h(4))
+    
